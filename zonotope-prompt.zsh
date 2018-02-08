f320e75fe87f393dd82eb9324dd8d205fa324a4a
ben lamothe <zonotope@gmail.com>
2018-02-08 09:32:14 -0500

    switch from '_' to '-' for untracked files

--- zonotope-prompt.zsh
+++ zonotope-prompt.zsh
@@ -75,11 +75,7 @@ function +vi-git-st() {
 +vi-git-untracked(){
     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
            git status --porcelain | grep '??' &> /dev/null ; then
-        # This will show the marker if there are any untracked files in repo.
-        # If instead you want to show the marker only if there are untracked
-        # files in $PWD, use:
-        #[[ -n $(git ls-files --others --exclude-standard) ]] ; then
-        hook_com[unstaged]+='_'
+        hook_com[unstaged]+='-'
     fi
 }
 
