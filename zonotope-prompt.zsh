fdd818ab60c49b841082aa7fb54c3bd7feb86ae6
ben lamothe <zonotope@gmail.com>
2018-02-08 09:27:23 -0500

    add hook to show if there are untracked files

--- zonotope-prompt.zsh
+++ zonotope-prompt.zsh
@@ -71,8 +71,20 @@ function +vi-git-st() {
     fi
 }
 
+## show "_" when there are untracked files
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+           git status --porcelain | grep '??' &> /dev/null ; then
+        # This will show the marker if there are any untracked files in repo.
+        # If instead you want to show the marker only if there are untracked
+        # files in $PWD, use:
+        #[[ -n $(git ls-files --others --exclude-standard) ]] ; then
+        hook_com[unstaged]+='_'
+    fi
+}
+
 ## set the prompt hooks
-zstyle ':vcs_info:git*+set-message:*' hooks git-st
+zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
 
 ############################################################################
 # prompt                                                                   #
